06aa009d2214efd915de8fad33585254136a93a2
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2023-06-06 17:00:44 -0300

    :sparkles: feat: creating routes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 import http from "node:http";
 
 const server = http.createServer((request, response) => {
+  const { method, url } = request;
+
+  if (method === "GET" && url === "/users") {
+    return response.end("Users list");
+  }
+
+  if (method === "POST" && url === "/users") {
+    return response.end("Creating new user");
+  }
+
   return response.end("Hello world!");
 });
 
